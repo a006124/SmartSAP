b2efcb774ac7036b5977f22193e5d5e53ca3e04a
a006124 <93441431+a006124@users.noreply.github.com>
2026-03-01 09:24:30 +0100

    Add files via upload

--- ViewModels/Modules/Module01ViewModel.cs
+++ ViewModels/Modules/Module01ViewModel.cs
@@ -15,10 +15,10 @@ namespace SmartSAP.ViewModels.Modules
         {
             Steps = new ObservableCollection<WorkflowStep>
             {
-                new WorkflowStep { Title = "1. Saisie des données de base", Description = "Renseigner l'identification, la catégorie et le constructeur dans le modèle Excel.", Icon = "\xE70F" },
-                new WorkflowStep { Title = "2. Données d'organisation", Description = "Affecter le centre de coûts, l'entreprise et les domaines d'activité.", Icon = "\xE762" },
-                new WorkflowStep { Title = "3. Intégration SAP (BAPI)", Description = "Appel de la BAPI_EQUI_CREATE pour générer les équipements.", Icon = "\xE8A5" },
-                new WorkflowStep { Title = "4. Audit & Validation", Description = "Vérification des numéros d'équipements générés et logs.", Icon = "\xE9A1" }
+                new WorkflowStep { Title = "1. Saisie des données de base", Description = "Crée un nouveau fichier Excel à renseigner à partir d'un modèle.", Icon = "\xE70F" },
+                new WorkflowStep { Title = "2. Sélection du fichier de données", Description = "Sélectionne le fichier Excel contenant les données à charger.", Icon = "\xE762" },
+                new WorkflowStep { Title = "3. Intégration SAP", Description = "Exécute la transaction SAP pour créer les Postes Techniques.", Icon = "\xE8A5" },
+                new WorkflowStep { Title = "4. Audit & Validation", Description = "Vérification la création des Postes Techniques et logs.", Icon = "\xE9A1" }
             };
         }
     }
