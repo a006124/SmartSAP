08297959fec2d76032611a98dd897db3715b7af4
a006124 <93441431+a006124@users.noreply.github.com>
2026-03-03 23:08:29 +0100

    Add files via upload

--- Views/Modules/Module05View.xaml.cs
+++ Views/Modules/Module05View.xaml.cs
@@ -8,5 +8,40 @@ namespace SmartSAP.Views.Modules
         {
             InitializeComponent();
         }
+
+        private void LogSection_DragEnter(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
+            {
+                e.Effects = System.Windows.DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = System.Windows.DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void LogSection_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(System.Windows.DataFormats.FileDrop);
+                if (files != null && files.Length > 0)
+                {
+                    string droppedFile = System.Linq.Enumerable.FirstOrDefault(files, f => f.EndsWith(".xlsx", System.StringComparison.OrdinalIgnoreCase) || f.EndsWith(".xls", System.StringComparison.OrdinalIgnoreCase));
+                    
+                    if (!string.IsNullOrEmpty(droppedFile))
+                    {
+                        var viewModel = this.DataContext as SmartSAP.ViewModels.Modules.ModuleDetailViewModelBase;
+                        if (viewModel != null)
+                        {
+                            viewModel.HandleDroppedExcelFile(droppedFile);
+                        }
+                    }
+                }
+            }
+            e.Handled = true;
+        }
     }
 }
